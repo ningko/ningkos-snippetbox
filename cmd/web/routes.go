055ff1f48cd4a837ac79f858fa751711e243c57e
Martina Garcia <mgarcia.06@outlook.es>
2025-04-18 19:36:50 -0300

    add files up to chapter 7

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/justinas/alice"
+)
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
+	standardMiddlware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -11,6 +15,6 @@ func (app *application) routes() *http.ServeMux {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return mux
+	return standardMiddlware.Then(mux)
 }
 
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,16 +3,27 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 	"ningko.net/snippetbox/pkg/models"
 )
 
 type templateData struct {
+	CurrentYear int
 	Snippet *models.Snippet
 	Snippets []*models.Snippet
 }
 
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap {
+	"humanDate": humanDate,
+}
+
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
+
 	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
 	if err != nil {
 		return nil, err
@@ -20,10 +31,11 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 
 	for _, page := range pages {
 		name := filepath.Base(page)
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
+
 
 		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
 		if err != nil {
@@ -34,7 +46,9 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 		if err != nil {
 			return nil, err
 		}
+
 		cache[name] = ts
 	}
+
 	return cache, nil
 }
